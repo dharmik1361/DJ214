2182788324eb82ca15a3c52e6214805fad8ffc24
__dharmik__joshi <63489702+dharmik1361@users.noreply.github.com>
2023-02-22 21:36:24 +0530

    Add files via upload

---
+++ pat10.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,a;
+  printf("\nEnter the value:");
+  scanf("%d",&a);
+  for(i=1;i<=a;i++)
+  {
+     printf("\n");
+     for(j=a-i+1;j>0;j--)
+     {
+       printf("%d",j);
+     }
+  }
+  return 0;
+}
