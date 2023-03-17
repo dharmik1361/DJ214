b516faf8f24fa0d05a41b984522f69b0dac5a46a
__dharmik__joshi <63489702+dharmik1361@users.noreply.github.com>
2023-03-17 18:29:03 +0530

    Add files via upload

---
+++ shoping/harihar.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title> </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+        
+
+
+<nav class="navbar navbar-dark bg-secondary">
+         <a class="navbar-brand" href="#">
+             <img src="image/logo.jfif" width="30" height="30" class="d-inline-block align-top" alt="">
+                 trak of maharastra
+            </a>
+            </nav>
+            <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/h4.jpg" alt="Los Angeles" >
+                <div class="carousel-caption">
+                <h3>harihar fort</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="image/t.jpg" alt="Chicago">
+                <div class="carousel-caption">
+                    
+                    
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="image/h6.jpg" alt="New York">
+                <div class="carousel-caption">
+                    
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+
+
+</body>
+</html>
---
+++ shoping/kalavati.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title> </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+
+    </nav>
+
+    <nav class="navbar navbar-dark bg-secondary">
+        <a class="navbar-brand" href="#">
+            <img src="image/logo.jfif" width="30" height="30" class="d-inline-block align-top" alt="">
+            trak of maharastra
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                </li>
+    </nav>
+
+    <div id="demo" class="carousel slide" data-ride="carousel">
+        <ul class="carousel-indicators">
+            <li data-target="#demo" data-slide-to="0" class="active"></li>
+            <li data-target="#demo" data-slide-to="1"></li>
+            <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/fort.jpeg" alt="Los Angeles">
+                <div class="carousel-caption">
+                    <h3>kalavati fort</h3>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="image/hfor.jpg" alt="Chicago">
+                <div class="carousel-caption">
+
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="image/fort2.jpg" alt="New York">
+                <div class="carousel-caption">
+
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+    <section class="my-5">
+        <div class="py-5">
+
+            <div>
+                <h2 class="text-center"> about</h2>
+            </div>
+            <div>
+                <div class="row justify-content-around">
+                    <div class="col-lg-3 col-md-3 col-12">
+                        <img src="image/kl2.jfif" class="img-fluid aboutimg">
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <h2 class="display-4"> kalavati </h2>
+                        <p class="py-5">Kalavantin Durg is a 2,250 ft (686 m) high summit located in the Western Ghats,
+                            near the Prabalgad fort in the Raigad district of Maharashtra, India. It is also known as
+                            Kelve Teen, Kalavantinicha Sulka, or Kalavantin Pinnacle; alternative transliterations
+                            include Kalawantin, Kalavati, and Kalavanti. It is a popular trekking destination. </p>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <section class="my-5">
+                <div class="py-5">
+
+                    <div>
+                        <h2 class="text-center"> Gallary </h2>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                        <div class="col-lg-4 col-md-4 col-12">
+                        <img src="image/kl2.jfif"class="row justify-content-around" height="500" width="250" >
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <img src="image/kl3.jfif"class="row justify-content-around" >
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-12">
+                        <img src="image/kl4.jpg"class="row justify-content-around" >
+                    </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </section>
+</body>
+
+</html>
